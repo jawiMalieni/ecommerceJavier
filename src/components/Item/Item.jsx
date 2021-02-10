d0050d2c85d56357a2898f35a0b46893aa146162
Javier H Malieni <casamalieni@gmail.com>
2021-02-09 21:02:25 -0300

    estylos card

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,13 +1,20 @@
+import {Card, ListGroup} from 'react-bootstrap';
 
-
-const Item = ({product}) => {
+const Item = ({ product }) => {
     return (
-        <div>
-        <h3>{product.name}</h3>
-        <p>Price: {product.price}</p>
-        <p>Stock: {product.Stock}</p>    
-        </div>
-    )
-}
+     //   <div>
+     //   <h3>{product.name}</h3>
+     //   <p>Price: {product.price}</p>
+     //   <p>Stock: {product.Stock}</p>    
+     //   </div>
+        <Card style={{ width: '18rem' }}>
+        <Card.Header>{product.name}</Card.Header>
+          <ListGroup variant="flush">
+          <ListGroup.Item>Price: {product.price}</ListGroup.Item>
+         <ListGroup.Item>{product.Stock}</ListGroup.Item>
+        </ListGroup>
+        </Card>
+    );
+};
 
 export default Item
--- src/components/mocks/productsList.jsx
+++ src/components/mocks/productsList.jsx
@@ -1,8 +1,8 @@
 const productList = [
-{ id: 1, name: "celular", price: 15000, stock: 10 },
-{ id: 2, name: "computadora", price: 98000, stock: 19 },
+{ id: 1, name: "celular", price: 15000, stock: 100},
+{ id: 2, name: "computadora", price: 98000, stock: 190 },
 { id: 3, name: "TV", price: 25000, stock: 100 },
-{ id: 4, name: "notebook", price: 150000, stock: 50 },
-{ id: 5, name: "consola", price: 95000, stock: 40 },
+{ id: 4, name: "notebook", price: 150000, stock: 500 },
+{ id: 5, name: "consola", price: 95000, stock: 400 },
 ];
 export default productList;
