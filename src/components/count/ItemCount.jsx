9ccc0eeb9f04bac455712ca7a16e0704d088aff1
Javier H Malieni <casamalieni@gmail.com>
2021-02-16 22:00:51 -0300

    ajuste contador

--- src/components/count/ItemCount.jsx
+++ src/components/count/ItemCount.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 
-const ItemCount = ({stock,initial}) => {
+const ItemCount = ({stock,initial, onAdd}) => {
 
     const [count, setCount] = useState(initial);
 
